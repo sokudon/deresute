43cbb7b2d36e5f035b95f764814e4e0822ffe1ae
sokudon <tdty15@gmail.com>
2020-03-01 15:01:34 +0900

    parede utc 対応（）

--- dt_json/cgss_pare_high_title.js
+++ dt_json/cgss_pare_high_title.js
@@ -17,13 +17,19 @@ var base_year=2019;//ぼだ開始年
 var BYEAR="2019";
 var BYEARN="";
 
-//2019/11/26　https://script.googleusercontent.com/macros/echo?user_content_key=lki_a2OA4PfcQgrTdxvgm-sDFVs0xqpZdVh0_9upcEyHpWlU3pj3eTFlEOjA3Vdgl_U6PaP7w1SlvJZq2B922Bf5wX2liHefm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnEdYs-SHhnypKUTiEZm3SCArUXFrY-Pqes1qZkWvFnS65t53M_2aEoWlR3RThGMpelrAtyz7VYaP&lib=M8drSt-VyvTtx6tm2NqCpPnndBJ9ix56c
+var timezone =-((new Date("2014-01-01T00:00:00Z")).getTimezoneOffset()/60)*3600*1000;//2014時　windowsの時計
+var jst =9*3600*1000;
+
+
+//2019/11/26　https://script.googleusercontent.com/macros/echo?user_content_key=YTZcFok8T7oOnIKYUDmOdJqiOvPHmTkhAnQJl5N-Zc0_DtvXZtBSwnot7ZcwNQQ3KstN3xy0A8XluPX0Uxy838A1gQSDhYfmm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnBT4TQs36VO96QWKkeEvLb4kY7NZNTJJI6nWH3wTET7baA6SKyZsjrbCeKHyoiDHCOrH0QulUKuK&lib=Mz9c-W5b2oX_w2vwGZs-LgHndBJ9ix56c
 //ぐぐるからしゅとく
 var tyukanend	= new Date(2019,3,25,15,0);//いべんと開始日時 月-1
-var ibe_kaishi	=moment(ibekaishi).add('hours',9);//いべんと開始日時 月-1
-var ibe_end		=moment(ibeowari).add('hours',9);;//いべんと終了日時 月-1
+var ibe_kaishi	=moment(ibekaishi).utc();//いべんと開始日時 月-1
+var ibe_end		=moment(ibeowari).utc();//いべんと終了日時 月-1
 
-var ibe_owari=ibe_end - ibe_kaishi+Date.UTC(2014,0,1,-9,0);
 
+//2019/11/20 15:00	2019/11/27 21:00 mente 28
+
+var ibe_owari=ibe_end - ibe_kaishi+Date.UTC(2014,0,1,0,0)-timezone;
 
 var dtd=1;//つーるちっぷ;０でいべんと日付のみ　こんぺあは経過時間のみ、1で両方表示
