34abac5ddd8b21b9c7af1210b0b995f01202604c
sokudon <tdty15@gmail.com>
2020-12-17 15:43:46 +0900

    ぱいそんのたいまー更新

--- ぱいそんのたいまー.py
+++ ぱいそんのたいまー.py
@@ -1,67 +1,63 @@
-#ぱいそんのたいまー
+from tkinter import *
+import time
 import datetime
-import tkinter
 import math
+#ぱいそんのたいまー
 
-root = tkinter.Tk()
-root.title("netaたいま")
-root.geometry("300x300")
+root = Tk()
+
+buff = StringVar()
+buff.set('')
+Label(textvariable = buff).pack()
+
 
 ibe='ぷろせか'
-s = '2020-12-10T15:00:00+09:00'
+s = '2020-12-10T14:00:00+08:00'
 ss = '2020-12-18T12:00:00Z'
-timefm='%Y/%m/%d %H:%M'
-
+timefm='%Y/%m/%d %H:%M:%S %Z'
 
 s=s.replace('Z', '+00:00')
 ss=ss.replace('Z', '+00:00')
 
-now = datetime.datetime.now()
 st  = datetime.datetime.fromisoformat(s)
 en  = datetime.datetime.fromisoformat(ss)
 
-
-tm = now.strftime(timefm)
-
-tmp=st.timestamp()
-st=datetime.datetime.fromtimestamp(tmp)
+sttmp=st.timestamp()
+st=datetime.datetime.fromtimestamp(sttmp)
 ts = st.strftime(timefm)
-
-#utcをJSTに変換　エポック秒にして再変換
-tmp=en.timestamp()
-en=datetime.datetime.fromtimestamp(tmp)
+entmp=en.timestamp()
+en=datetime.datetime.fromtimestamp(entmp)
 te = en.strftime(timefm)
-
 span= abs(en-st)
-elapsed= abs(now-st)
-left= abs(en-now)
-
-# <class 'datetime.timedelta'>
-x = abs(now-st)/abs(en-st)*100
-n = 2  
-y = math.floor(x * 10 ** n) / (10 ** n)
-progress = y
 
 
-# ラベルの作成
-labelt = tkinter.Label(root, text=ibe)
-label = tkinter.Label(root, text=tm)
-label2 = tkinter.Label(root, text=ts)
-label3 = tkinter.Label(root, text=te)
-label4 = tkinter.Label(root, text=span)
-label5 = tkinter.Label(root, text=elapsed)
-label6 = tkinter.Label(root, text=left)
-label7 = tkinter.Label(root, text=progress)
+def dtime(dt):
+    if dt<0:
+            return "0日0時間0分"
+    dt=abs(dt)
+    seconds  = math.floor((dt / 10) % 60)
+    minutes  = math.floor((dt / 60) % 60)
+    hours    = math.floor((dt / 3600) % 24)
+    days     = math.floor(dt / 86400)
+    tmp = str(days) +"日" +str(hours)+"時間"+str(minutes) +"分"
+    return tmp
 
-#ラベルの表示
-labelt.grid()
-label.grid()
-label2.grid()
-label3.grid()
-label4.grid()
-label5.grid()
-label6.grid()
-label7.grid()
+# 時刻の表示
+def show_time():
+    nn=time.time()
+    elapsed=dtime(nn-sttmp)
+    left= dtime(entmp-nn)
+    x = abs(nn-sttmp)/abs(entmp-sttmp)*100
+    n = 2
+    y = math.floor(x * 10 ** n) / (10 ** n)
+    progress = y
+    if y>100:
+             y=100
+    tm=time.strftime(timefm)
+    buff.set(ibe +'\n'+ tm+'\n' +ts +'\n'+te+"\n" +str(span)+"\n"+elapsed+"\n"+left+"\n"+str(y)+"%")
+    root.after(1000, show_time)
+    #1000msの後にshow_time関数を呼び出しています
 
 
+show_time()
 root.mainloop()
